17d0cfc589c0b21b4a3ab8141dc2ea9725e93a69
mingngocsinhgai2002-pixel <mingngocsinhgai2002@gmail.com>
2026-03-10 08:12:23 +0700

    Updated app.json

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
+import Constants from 'expo-constants';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = Constants.expoConfig?.extra?.supabaseUrl || 'https://seoxgqnatjmxcnxzzumc.supabase.co';
+const supabaseAnonKey = Constants.expoConfig?.extra?.supabaseAnonKey || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlb3hncW5hdGpteGNueHp6dW1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5NzY5MDAsImV4cCI6MjA4NTU1MjkwMH0.6aIcvU41x3TU4DmmODesD4K2RApQTOMpr5H3p5cPtf0';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
